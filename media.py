0921f3d2f00bc86c7078f7cece3913e7eeefd865
Surya Dasari <38848866+dasarisurya@users.noreply.github.com>
2018-05-20 18:53:07 +0530

    commented lines and updated

--- media.py
+++ media.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
+import webbrowser
 print("Content-type:text/html \n")
 
-import webbrowser
 
 class Movie():
+    '''
+      movie class is defined
+    '''
 
-    def __init__(self,movieName,
-                 movieStoryline, posterImage,trailerYoutube):
+    def __init__(self, movieName,
+                 movieStoryline, posterImage, trailerYoutube):
+        '''movie content'''
         self.title = movieName
         self.storyline = movieStoryline
         self.poster_image_url = posterImage
         self.trailer_youtube_url = trailerYoutube
 
     def show_trailer(self):
+        '''trailer opens in default browser in the same window'''
         webbrowser.open(self.trailer_youtube_url)
